7fc7b526f971a101daca7a1eb99d5bb281a5bc63
FabrizioLeopardi <s4816319@studenti.unige.it>
2024-01-10 19:02:34 +0100

    first commit

---
+++ ros_ws/build/second_rosplan_interface/CMakeFiles/myaction2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/root/Desktop/ros_ws/devel/lib/second_rosplan_interface/myaction2"
+  "/root/Desktop/ros_ws/devel/lib/second_rosplan_interface/myaction2.pdb"
+  "CMakeFiles/myaction2.dir/src/my_action2.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myaction2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ros_ws/build/second_rosplan_interface/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/Desktop/ros_ws/src/second_rosplan_interface
+# Build directory: /root/Desktop/ros_ws/build/second_rosplan_interface
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
